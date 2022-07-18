40f9eeab88c375aa709ecfaf1293379770700765
sajawalhassan <scientificninja1f12@gmail.com>
2022-07-18 12:38:51 +0500

    Pull data from sanity

--- app/src/components/FeaturedRow.js
+++ app/src/components/FeaturedRow.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { View, Text, ScrollView } from "react-native";
 import { ArrowRightIcon } from "react-native-heroicons/outline";
 
 import ResurantCard from "./ResurantCard";
+import sanityClient from "../../sanity";
 
 const FeaturedRow = ({ title, description, id }) => {
+  const [resturants, setResturants] = useState([]);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `
+    *[_type == "featured" && _id == $id] {
+  ...,
+  resturants[] -> {
+    ...,
+    dishes[] ->,
+    type => {
+      name
+    }
+  }
+}[0]
+    `,
+        { id: id }
+      )
+      .then((data) => setResturants(data.resturants));
+  }, []);
+
   return (
     <View>
       <View className="mt-4 flex-row items-center justify-between px-4">
@@ -21,6 +44,24 @@ const FeaturedRow = ({ title, description, id }) => {
         showsHorizontalScrollIndicator={false}
         className="pt-4"
       >
+        {resturants.map((resturant) => (
+          <ResurantCard
+            key={resturant._id}
+            id={resturant._id}
+            name={resturant.name}
+            imgUrl={resturant.image}
+            address={resturant.address}
+            title={resturant.name}
+            dishes={resturant.dishes}
+            genre={resturant.type.name}
+            description={resturant.short_description}
+            rating={resturant.rating}
+            type={resturant.type.name}
+            long={resturant.long}
+            lat={resturant.lat}
+          />
+        ))}
+        {/* 
         <ResurantCard
           id={123}
           imgUrl="https://links.papareact.com/gn7"
@@ -68,7 +109,7 @@ const FeaturedRow = ({ title, description, id }) => {
           dishes={["sushi", "sashimi", "tempura"]}
           long={20}
           lat={0}
-        />
+        /> */}
       </ScrollView>
     </View>
   );
--- app/src/screens/HomeScreen.js
+++ app/src/screens/HomeScreen.js
@@ -1,21 +1,39 @@
 import { SafeAreaView, Platform, ScrollView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import { useLayoutEffect } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 
 import React from "react";
 import Header from "../components/Header";
 import Categories from "../components/Categories";
 import FeaturedRow from "../components/FeaturedRow";
+import sanityClient from "../../sanity";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const [featuredCategories, setFeaturedCategories] = useState([]);
 
   useLayoutEffect(() => {
     // Removing the React Navigation header
     navigation.setOptions({
       headerShown: false,
     });
+  }, []);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `
+          *[_type == "featured"] {
+              ...,
+            resturants[]->{
+              ...,
+              dishes[]->
+            }
+            }
+    `
+      )
+      .then((data) => setFeaturedCategories(data));
   }, []);
 
   return (
@@ -29,23 +47,14 @@ const HomeScreen = () => {
       >
         <Categories />
 
-        <FeaturedRow
-          id="123"
-          title="Featured"
-          description="Paid placements from out pertners"
-        />
-
-        <FeaturedRow
-          id="456"
-          title="Tasty Discounts!"
-          description="Everyone's been enjoying these juicy discounts!"
-        />
-
-        <FeaturedRow
-          id="789"
-          title="Offer's near you"
-          description="Why not support your local businesses?"
-        />
+        {featuredCategories?.map((category) => (
+          <FeaturedRow
+            key={category._id}
+            id={category._id}
+            title={category.name}
+            description={category.short_description}
+          />
+        ))}
       </ScrollView>
       <StatusBar style="auto" />
     </SafeAreaView>
